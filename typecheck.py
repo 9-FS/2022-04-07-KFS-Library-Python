3bbe55e5807fa46bb9a6af705525602e4d311832
9FS <pray4spam@googlemail.com>
2022-10-19 10:30:14 +0000

    added typecheck instance mode

--- typecheck.py
+++ typecheck.py
@@ -5,15 +5,16 @@ import typing
 
 class Mode(Enum):   #Typcheckmodi
     strict=0        #Argumenttyp muss Typtipp sein
-    convertable=1   #Argumenttyp muss konvertierbar zu Typtipp sein
-    whatever=2      #Argumenttyp ist egal
+    instance=1      #Argumenttyp muss Typtipp oder Unterklasse sein (Standard)
+    convertable=2   #Argumenttyp muss konvertierbar zu Typtipp sein
+    whatever=3      #Argumenttyp ist egal
 
 
-def check(f: typing.Callable, locals: dict, *modes: Mode) -> None:
-    #if type(f)==function: TODO funktioniert so nicht
-    #    raise ValueError("Error in KFS::typecheck::check(...): Argument f must be callable, use your local function.")
-    if type(locals)!=dict:
-        raise ValueError("Error in KFS::typecheck::check(...): Argument locals must be of type dict, use the locals() function.")
+def check(f: typing.Callable, locals: dict, *modes: Mode) -> None:  #TODO typecheck nested types; TODO make compatible with decorators
+    if isinstance(f, typing.Callable)==False:
+        raise ValueError("Error in KFS::typecheck::check(...): Argument f must be callable, use your local function.")
+    if isinstance(locals, dict)==False:
+        raise ValueError("Error in KFS::typecheck::check(...): Argument locals must be instance of type dict, use the locals() function.")
     for mode in modes:
         if type(mode)!=Mode:
             raise ValueError("Error in KFS::typecheck::check(...): Given modes must be of type KFS::typecheck::Mode.")
@@ -33,13 +34,17 @@ def check(f: typing.Callable, locals: dict, *modes: Mode) -> None:
     for i, parameter in enumerate(type_hints):  #alle Datentyptipps durchgehen und schauen, ob Parameter Anforderung entspricht
         if modes[i]==Mode.strict:                               #wenn Modus streng:
             if type(locals[parameter])!=type_hints[parameter]:  #Parametertyp muss Datentipptyp sein
-                raise TypeError(f"Error in {f.__name__}{inspect.signature(f)}: {parameter} must be of type {type_hints[parameter]}, but {type(locals[parameter])} was given.")
+                raise TypeError(f"Error in {f.__name__}{inspect.signature(f)}: {parameter} must be of type {type_hints[parameter]}, but {type(locals[parameter])}={locals[parameter]} was given.")
+        
+        elif modes[i]==Mode.instance:                                       #wenn Modus Instanz:
+            if isinstance(locals[parameter], type_hints[parameter])==False: #Parametertyp muss Datentipptyp oder Unterklasse sein
+                raise TypeError(f"Error in {f.__name__}{inspect.signature(f)}: {parameter} must be instance of type {type_hints[parameter]}, but {type(locals[parameter])}={locals[parameter]} was given.")
         
         elif modes[i]==Mode.convertable:    #wenn Modus Parametertyp muss konvertierbar sein zu Datentipptyp:
             try:
                 type_hints[parameter](locals[parameter])    #Konvertierung versuchen von Parametertyp zu Datentipptyp
             except ValueError:
-                raise TypeError(f"Error in {f.__name__}{inspect.signature(f)}: {parameter} must be convertable to type {type_hints[parameter]}, but {type(locals[parameter])} was given.")
+                raise TypeError(f"Error in {f.__name__}{inspect.signature(f)}: {parameter} must be convertable to type {type_hints[parameter]}, but {type(locals[parameter])}={locals[parameter]} was given.")
         
         elif modes[i]==Mode.whatever:   #wenn Modus egal: ignorieren
             pass
