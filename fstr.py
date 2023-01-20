a44c6ed90ddd554332e0d324d79b96f551ac4b36
9FS <pray4spam@googlemail.com>
2023-01-20 13:18:27 +0000

    added KFS::fstr::notation_abs(...)

--- fstr.py
+++ fstr.py
@@ -3,6 +3,38 @@ from . import math as KFSmath   #for round_sig, must "as KFSmath" because otherw
 from . import types
 
 
+def notation_abs(x: float, precision: int, round_static: bool=False, trailing_zeros: bool=True) -> str: #converts to formatted rounded number (notation absolute) as string, no changing of magnitude for decimal prefixes
+    types.check(notation_abs, locals(), types.Mode.convertable, types.Mode.instance, types.Mode.instance, types.Mode.instance)
+    x=float(x)
+
+    
+    if round_static==False:
+        x=KFSmath.round_sig(x, precision)           #round to signifcant number
+    else:
+        x=round(x, precision)                       #round to decimal place static
+        
+    if x!=0:                                        #determine magnitude after rounding in case rounding changes magnitude
+        magnitude=math.floor(math.log10(abs(x)))    #x magnitude floored
+    else:
+        magnitude=0                                 #for number 0 magnitude 0, practical for decimal prefix
+    
+    if round_static==False:
+        dec_places=magnitude*-1+precision-1         #decimal places required
+    else:
+        dec_places=precision                        #decimal places required
+    if dec_places<0:                                #at least 0 decimal places
+        dec_places=0
+
+
+    x=f"{x:,.{dec_places}f}".replace(".", "%TEMP%").replace(",", ".").replace("%TEMP%", ",")   #int to str, comma as decimal separator
+    
+    if trailing_zeros==False and "," in x:  #if trailing zeros undesired and decimal places existing:
+        x=x.rstrip("0")                     #remove trailing zeros
+        if x[-1]==",":                      #if because of that last character comma:
+            x=x[:-1]                        #remove comma
+
+    return x
+
 def notation_tech(x: float, precision: int, round_static: bool=False, trailing_zeros: bool=True, add_decimal_prefix: bool=True) -> str: #converts to notation technical as string
     types.check(notation_tech, locals(), types.Mode.convertable, types.Mode.instance, types.Mode.instance, types.Mode.instance, types.Mode.instance)
     x=float(x)
@@ -16,7 +48,7 @@ def notation_tech(x: float, precision: int, round_static: bool=False, trailing_z
     if x!=0:                                        #determine magnitude after rounding in case rounding changes magnitude
         magnitude=math.floor(math.log10(abs(x)))    #x magnitude floored
     else:
-        magnitude=0                                 #for number 0 magnitude 0 practical for decimal prefix
+        magnitude=0                                 #for number 0 magnitude 0, practical for decimal prefix
     
     if round_static==False:
         dec_places=magnitude%3*-1+precision-1       #decimal places required
@@ -34,7 +66,11 @@ def notation_tech(x: float, precision: int, round_static: bool=False, trailing_z
             x=x[:-1]                        #remove comma
     
     if add_decimal_prefix==True:    #if decimal prefix desired: append
-        if    24<=magnitude and magnitude< 27:
+        if    30<=magnitude and magnitude< 33:
+            x+="Q"
+        elif  27<=magnitude and magnitude< 30:
+            x+="R"
+        elif  24<=magnitude and magnitude< 27:
             x+="Y"
         elif  21<=magnitude and magnitude< 24:
             x+="Z"
@@ -68,6 +104,10 @@ def notation_tech(x: float, precision: int, round_static: bool=False, trailing_z
             x+="z"
         elif -24<=magnitude and magnitude<-21:
             x+="y"
+        elif -27<=magnitude and magnitude<-24:
+            x+="r"
+        elif -30<=magnitude and magnitude<-27:
+            x+="q"
         else:
             raise ValueError(f"Error in KFS::fstr::notation_tech(...): There are only decimal prefixes for magnitudes [-24; 27[. There is no decimal prefix for given magnitude {magnitude}.")
         
