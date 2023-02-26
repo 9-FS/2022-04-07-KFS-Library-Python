fb4146254336e0cc630452688bb2d55d948cedc4
9FS <pray4spam@googlemail.com>
2023-02-26 18:55:37 +0000

    added support for import KFS (top level package)

---
+++ __init__.py
@@ -0,0 +1,20 @@
+from . import config
+from . import dropbox
+#from . import DFS_AIP
+from . import dropbox
+from . import exceptions
+from . import fstr
+from . import log
+from . import math
+from . import media
+
+__all__=[
+    "config",
+    #DFS_AIP,
+    "dropbox",
+    "exceptions",
+    "fstr",
+    "log",
+    "math",
+    "media"
+]
